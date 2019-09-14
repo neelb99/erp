b5ead18dc9ac55953d0bec57bc9c2c178e26b83f
neelb99 <neelbajaj1234@hotmail.com>
2019-09-15 02:13:17 +0530

    home page text

--- client/src/components/Home.component.js
+++ client/src/components/Home.component.js
@@ -21,7 +21,7 @@ const Home = ()=>{
         <div className="landing row text-center">
             <div className="col-12 col-md-6" id="intro">
                 <h1>Sample ERP</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p>Welcome to Sample ERP. Please log in to use the system. This system is designed for owners, employees and customers and can be used by any of them using the username and password provided by the administrator. Every user will have different permissions based on their role in the organization. The administrator can create new accounts for new managers, employees or customers.</p>
             </div>
             <div className="col-12 col-md-6" id="login">
                 <Loginform/>
